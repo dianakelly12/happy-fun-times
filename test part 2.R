f18cb7dfffca3d45d3f4a0e6a2fd72f49d784a7b
Diana Kelly <dkelly13@nd.edu>
2021-08-18 15:11:11 -0400

    practice

--- test part 2.R
+++ test part 2.R
@@ -1,1 +1,25 @@
-test 
+install.packages(c("car", "ggplot2"))
+library(car)
+car::scatterplot(mpg~disp, data=mtcars)
+plot(mtcars$disp, mtcars$mpg)
+abline(lm(mpg ~ disp, data = mtcars))
+car::scatterplot(mpg ~ disp, 
+                 data = mtcars)
+library(ggplot2)
+ggplot(data = mtcars, 
+       mapping = aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal ()
+
+install.packages('plotly')  
+plotly::ggplotly(my_plot)
+
+install.packages(c("readr", "data.table"))
+
+st_joe_land <- read.csv(file = "stJoeLand.csv")
+names(st_joe_land_datatable)[names(st_joe_land_datatable) == "Zip Code"] <- 
+  "Zip_Code"
+
+save(st_joe_land, st_joe_land_datatable, file = "stJoeInfo.RData")
+load("stJoeInfo.RData")
